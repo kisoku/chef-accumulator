4c7f0ecc5bd51fcda0ab92298cbe90efeaaf8950
Christian Höltje <docwhat@gerf.org>
2016-06-03 10:47:53 -0400

    Added meta info for supermarket
    This adds an issues and source link in Supermarket.

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,7 @@ maintainer       "Mathieu Sauve-Frankel"
 maintainer_email "msf@kisoku.net"
 license          "Apache 2.0"
 description      "Installs/Configures accumulator"
+source_url       "https://github.com/kisoku/chef-accumulator" if respond_to?(:source_url)
+issues_url       "https://github.com/kisoku/chef-accumulator/issues" if respond_to?(:issues_url)
 
 version          "0.2.0"
